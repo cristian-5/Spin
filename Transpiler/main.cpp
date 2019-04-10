b4ee304a5440e10574b1028c11d645fe4968413d
Cristian <Cristian@Host321.local>
2019-04-10 20:07:03 +0200

    Added Transpiler

---
+++ Transpiler/main.cpp
@@ -0,0 +1,33 @@
+
+/*!
+ *
+ *    + --------------------------------------- +
+ *    |  main.cpp                               |
+ *    |                                         |
+ *    |                  Main                   |
+ *    |                                         |
+ *    |  Created by Cristian A.                 |
+ *    |  Copyright © MIT. All rights reserved.  |
+ *    + --------------------------------------- +
+ *
+ *    Note: This software is licensed under
+ *          the (MIT) Massachusetts Institute
+ *          of Technology License.
+ *
+ */
+
+#include <iostream>
+
+#include "transpiler.h"
+
+using namespace std;
+
+using namespace StackTranspiler;
+
+int main(int argc, const char * argv[]) {
+	
+	
+	
+	
+	return EXIT_SUCCESS;
+}
---
+++ Transpiler/transpiler.h
@@ -0,0 +1,44 @@
+
+/*!
+ *
+ *    + --------------------------------------- +
+ *    |  transpiler.h                           |
+ *    |                                         |
+ *    |               Transpiler                |
+ *    |                                         |
+ *    |  Created by Cristian A.                 |
+ *    |  Copyright © MIT. All rights reserved.  |
+ *    + --------------------------------------- +
+ *
+ *    Note: This software is licensed under
+ *          the (MIT) Massachusetts Institute
+ *          of Technology License.
+ *
+ */
+
+#ifndef TRANSPILER
+#define TRANSPILER
+
+#include <iostream>
+#include <string>
+
+/*! @brief Namespace Stack */
+namespace StackTranspiler {
+	
+	/*! @brief Transpiler Class. */
+	class Transpiler {
+		
+	private:
+		
+	public:
+		
+		Transpiler() { }
+		
+	};
+	
+	
+	
+}
+
+
+#endif
