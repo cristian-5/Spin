e2406a1ee4c21d33a676a72ac3f254f92e7854e3
Cristian <Cristian@Host321.local>
2019-04-10 19:36:42 +0200

    Added Transpiler

---
+++ Transpiler/Transpiler/main.cpp
@@ -0,0 +1,29 @@
+
+/*!
+ *
+ *    + --------------------------------------- +
+ *    |  main.cpp                               |
+ *    |                                         |
+ *    |                  Main                   |
+ *    |                                         |
+ *    |  Created by Cristian A.                 |
+ *    |  Copyright © MIT. All rights reserved.  |
+ *    + --------------------------------------- +
+ *
+ *    Note: This software is licensed under
+ *          the (MIT) Massachusetts Institute
+ *          of Technology License.
+ *
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+	
+	
+	
+	
+	return EXIT_SUCCESS;
+}
