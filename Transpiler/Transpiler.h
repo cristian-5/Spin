01b5350fdc98673391668e38af513f7f86846c4f
Cristian <Cristian@Host321.local>
2019-04-11 15:26:45 +0200

    Added Regexes

--- Transpiler/Transpiler.h
+++ Transpiler/Transpiler.h
@@ -21,8 +21,13 @@
 
 #include <iostream>
 #include <string>
+#include <regex>
+
+#include "HeapCollection.h"
 
 using namespace std;
+
+using namespace HeapCollection;
 
 /*! @brief Namespace Stack */
 namespace StackTranspiler {
@@ -30,8 +35,128 @@ namespace StackTranspiler {
 	/*! @brief Transpiler Class. */
 	class Transpiler {
 		
-		static string process(string code) {
-			return "";
+	private:
+		
+		/* MARK: - Regexes */
+		
+		/*!
+		 *   @brief Matches a regex.
+		 *   @param rgx Regex string.
+		 *   @param input Input string.
+		 *   @returns The matched string.
+		 */
+		string match(string rgx, string input) {
+			string result;
+			try {
+				regex regex(rgx);
+				smatch match;
+				regex_search(input, match, regex);
+				if (match.size() > 0) {
+					result = match.str(0);
+				} else return "";
+			} catch (regex_error & e) {
+				return "";
+			}
+			return result;
+		}
+		
+		/*!
+		 *   @brief Matches a regex and returns
+		 *   the first capture group.
+		 *   @param rgx Regex string.
+		 *   @param input Input string.
+		 *   @returns The first matched group.
+		 */
+		string matchClose(string rgx, string input) {
+			string result;
+			try {
+				regex regex(rgx);
+				smatch match;
+				regex_search(input, match, regex);
+				if (match.size() > 1) {
+					result = match.str(1);
+				} else return "";
+			} catch (regex_error & e) {
+				return "";
+			}
+			return result;
+		}
+		
+		/*!
+		 *   @brief Matches a regex and returns
+		 *   all captures group in a list.
+		 *   @param rgx Regex string.
+		 *   @param input Input string.
+		 *   @returns The matched groups.
+		 */
+		StrongList<string> matchGroupClose(string rgx, string input) {
+			StrongList<string> result;
+			try {
+				regex regex(rgx);
+				smatch match;
+				regex_search(input, match, regex);
+				if (match.size() > 1) {
+					for (int i = 1; i < match.size(); i++) {
+						result.link(match.str(i));
+					}
+				} else return StrongList<string>();
+			} catch (regex_error & e) {
+				return StrongList<string>();
+			}
+			return result;
+		}
+		
+		/*!
+		 *   @brief Matches a regex at the
+		 *   beginning of a string.
+		 *   @param rgx Regex string.
+		 *   @param input Input string.
+		 *   @returns The matched string.
+		 */
+		string matchStart(string rgx, string input) {
+			return match("^" + rgx, input);
+		}
+		
+		/*!
+		 *   @brief Matches a regex at the
+		 *   beginning of a string and returns
+		 *   the first capture group.
+		 *   @param rgx Regex string.
+		 *   @param input Input string.
+		 *   @returns The first matched group.
+		 */
+		string matchCloseStart(string rgx, string input) {
+			return matchClose("^" + rgx, input);
+		}
+		
+		/*!
+		 *   @brief Replaces every occurrence of the
+		 *   matched regex with the given string.
+		 *   @param rgx Regex string.
+		 *   @param input Input string.
+		 *   @param replace Input Replace.
+		 *   @returns The replaced string.
+		 */
+		string replaceMatches(string rgx, string input, string replace) {
+			regex regex(rgx);
+			string result = input;
+			while (regex_search(result, regex)) {
+				result = regex_replace(result, regex, replace);
+			}
+			return result;
+		}
+		
+	public:
+		
+		static string process(string & code) {
+			
+			Dictionary<regex, string> patterns = Dictionary<regex, string>();
+
+			patterns.link(regex("(?:^|[\s\t]+)int8[\s\t]+"), "byte");
+			patterns.link(regex("(?:^|[\s\t]+)Int8[\s\t]+"), "SByte");
+			
+			
+			return code;
 		}
 		
 	};
