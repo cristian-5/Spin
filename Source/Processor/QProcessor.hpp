fbf74d4d48a760c0de188ebd8d421e0098439d36
Cristian-A <cristianantonuccio@icloud.com>
2019-05-16 20:57:42 +0200

    Added Virtual Processors

---
+++ Source/Processor/QProcessor.hpp
@@ -0,0 +1,61 @@
+
+/*!
+ *
+ *    + --------------------------------------- +
+ *    |  QProcessor.hpp                         |
+ *    |                                         |
+ *    |            Quantum Processor            |
+ *    |                                         |
+ *    |  Created by Cristian A.                 |
+ *    |  Copyright © MIT. All rights reserved.  |
+ *    + --------------------------------------- +
+ *
+ *    Note: This software is licensed under
+ *          the (MIT) Massachusetts Institute
+ *          of Technology License.
+ *
+ */
+
+#ifndef STACKQPROCESSOR
+#define STACKQPROCESSOR
+
+#include <iostream>
+
+#include "../Aliases/Aliases.hpp"
+
+/*! @brief Namespace Stack. */
+namespace Stack {
+
+	/*!
+	 *   @brief Quantum Processor.
+	 *   This class emulates a 16 bits
+	 *   q processor and has its own
+	 *   Instruction Set.
+	 */
+	class QProcessor16 {
+
+		public:
+
+		VProcessor16() { }
+
+	};
+
+    /*!
+	 *   @brief Quantum Processor.
+	 *   This class emulates a 8 bits
+	 *   q processor and has its own
+	 *   Instruction Set.
+	 */
+	class QProcessor08 {
+
+		public:
+
+		VProcessor08() {
+
+		}
+
+	}
+
+}
+
+#endif
