ec28b1f0b747b2cfaca2c4a196236382c0ebd221
Cristian-A <cristianantonuccio@icloud.com>
2019-06-15 17:34:16 +0200

    Added Import Pass

--- Source/Transpiler/Transpiler.hpp
+++ Source/Transpiler/Transpiler.hpp
@@ -27,13 +27,40 @@
 #include "../Linker/FileHandler.hpp"
 #include "../Lexer/Lexer.hpp"
 
+#define TList StrongList<Token>
+
 namespace Stack {
+
+	/*!
+	 *   @brief Syntax Error Exception.
+	 *   Raised when a token is unexpected.
+	 */
+	class SyntaxErrorException: public Exception {
+		private:
+		String token = "";
+		String expected = "";
+		FilePosition pos = { 0, 0 };
+		String fileName = "";
+		public:
+		String getToken() { return token; }
+		String getExpected() { return expected; }
+		FilePosition getPosition() { return pos; }
+		String getFileName() { return fileName; }
+		SyntaxErrorException(
+			String t,
+			String e,
+			FilePosition position,
+			String name
+		):
+		Exception(), token(t), expected(e),
+		pos(position), fileName(name) { }
+	};
 
 	class Transpiler {
 
 		private:
 
-		String tokensToFile(StrongList<Token> * tokens) {
+		String tokensToFile(TList * tokens) {
 			if (tokens == nullptr) return "";
 			if (tokens -> isEmpty()) return "";
 			String file = "";
@@ -43,7 +70,7 @@ namespace Stack {
 			return file;
 		}
 
-		void removeBeginEnd(StrongList<Token> * tokens) {
+		void removeBeginEnd(TList * tokens) {
 			if (tokens -> isEmpty()) return;
 			if (tokens -> getNode(0).type == beginFile) {
 				tokens -> unlinkFirst();
@@ -54,9 +81,101 @@ namespace Stack {
 			}
 		}
 
-		void processTokens(StrongList<Token> * tokens) {
-			removeBeginEnd(tokens);
+		void processLibraries(TList * tokens) {
+			for (UInt32 i = 0; i < tokens -> count(); i++) {
+				Token t = tokens -> getNode(i);
+				if (t.type == libKeyword) {
+					t.value = "namespace";
+					tokens -> setNode(i, t);
+				}
+			}
+			for (UInt32 i = 0; i < tokens -> count(); i++) {
+				Token t = tokens -> getNode(i);
+				if (t.type == importKeyword) {
+					Token * token = getNext(tokens, i);
+					if (token != nullptr) {
+						i++;
+						token = getNext(tokens, i);
+						UInt32 idCount = 0;
+						while (token -> type == identifier) {
+							idCount++;
+							i++;
+							token = getNext(tokens, i);
+							if (token -> type == dot) {
+								token -> value = "::";
+								i++;
+								token = getNext(tokens, i);
+							} else if (token -> type == semicolon) {
+								break;
+							} else {
+								throw SyntaxErrorException(
+									token -> value, ";",
+									FilePosition(
+										getPosition(currentFile, token -> position)
+									),
+									* currentFileName
+								);
+							}
+						}
+						if (idCount == 0) {
+							throw SyntaxErrorException(
+								token -> value, "identifier",
+								FilePosition(
+									getPosition(currentFile, token -> position)
+								),
+								* currentFileName
+							);
+						}
+						if (token -> type == semicolon) {
+							if (idCount > 1) t.value = "using";
+							else t.value = "using namespace";
+						} else {
+							throw SyntaxErrorException(
+								token -> value, ";",
+								FilePosition(
+									getPosition(currentFile, token -> position)
+								),
+								* currentFileName
+							);
+						}
+					} else {
+						throw SyntaxErrorException(
+							"end of file", ";",
+							FilePosition(
+								getPosition(currentFile, t.position)
+							),
+							* currentFileName
+						);
+					}
+				}
+			}
 		}
+
+		Token * getNext(TList * tokens, UInt32 i) {
+			if (isNextAvailable(tokens, i)) {
+				Token * t = new Token();
+				* t =  tokens -> getNode(i + 1);
+				return t;
+			}
+			return nullptr;
+		}
+
+		Boolean isNextAvailable(TList * tokens, UInt32 i) {
+			i++; if (tokens -> isEmpty()) return false;
+			if (i >= tokens -> count()) return false;
+			return true;
+		}
+
+		void processTokens(TList * tokens) {
+			removeBeginEnd(tokens);
+			processLibraries(tokens);
+			// Include all files together
+			// Functions Procedures
+			// Entry Point
+		}
+
+		String * currentFileName = nullptr;
+		String * currentFile = nullptr;
 
 		public:
 
@@ -68,17 +187,20 @@ namespace Stack {
 			processFiles(files);
 		}
 
-		void processFiles(StrongList<String> files) {
+		void processFiles(StrongList<String> paths) {
 			Lexer lexer = Lexer();
-			for (UInt32 i = 0; i < files.count(); i++) {
+			for (UInt32 i = 0; i < paths.count(); i++) {
+				String path = paths[i];
 				String file = "";
 				try {
-					file = FileHandler::stringFromFile(files[i]);
-					StrongList<Token> * tokens = nullptr;
-					tokens = lexer.tokenize(& file);
+					file = FileHandler::stringFromFile(path);
+					currentFile = & file;
+					currentFileName = & path;
+					TList * tokens = nullptr;
+					tokens = lexer.tokenize(& file, path);
 					processTokens(tokens);
 					file = tokensToFile(tokens);
-					FileHandler::createNewFile(files[i] + ".cpp", file);
+					FileHandler::createNewFile(path + ".cpp", file);
 				} catch (Exception & e) { throw; }
 			}
 		}
