3bddb2051409eb00d71d2b44a72486a7fbd0a04a
Cristian-A <cristianantonuccio@icloud.com>
2019-07-03 14:39:27 +0200

    Created Variant

---
+++ Source/Token/Variant.hpp
@@ -0,0 +1,28 @@
+
+/*!
+ *
+ *    + --------------------------------------- +
+ *    |  Variant.hpp                            |
+ *    |                                         |
+ *    |                 Variant                 |
+ *    |                                         |
+ *    |  Created by Cristian A.                 |
+ *    |  Copyright © MIT. All rights reserved.  |
+ *    + --------------------------------------- +
+ *
+ *    Note: This software is licensed under
+ *          the (MIT) Massachusetts Institute
+ *          of Technology License.
+ *
+ */
+
+#ifndef STACKVARIANT
+#define STACKVARIANT
+
+#include "../Aliases/Aliases.hpp"
+
+namespace Stack {
+
+}
+
+#endif
