1311a0840bc68dc634df49351ec8f2db6c979c3f
Cristian-A <cristianantonuccio@icloud.com>
2019-08-15 16:49:04 +0200

    Parser Changes

--- Source/Parser/Parser.hpp
+++ Source/Parser/Parser.hpp
@@ -20,46 +20,62 @@
 #define STACKPARSER
 
 #include "../Aliases/Aliases.hpp"
+#include "../Linker/Linker.hpp"
 
 #include "ASTree.hpp"
-#include "Exceptions.hpp"
 
 namespace Stack {
+
+	class SyntaxError: public Exception {
+		private:
+		String message = "";
+		Linker::FilePosition pos = { 0, 0 };
+		public:
+		Linker::FilePosition getPosition() { return pos; }
+		String getMessage() { return message; }
+		SyntaxError(String m, Linker::FilePosition position):
+		Exception(), pos(position) { }
+	};
+
+	class ParseErrorException: public Exception {
+		private:
+		ArrayList<SyntaxError> * errors = nullptr;
+		String fileName = "";
+		public:
+		ArrayList<SyntaxError> * getErrors() { return errors; }
+		String getFileName() { return fileName; }
+		ParseErrorException(ArrayList<SyntaxError> * e, String f):
+		Exception(), errors(e), fileName(f) { }
+		~ParseErrorException() { delete errors; }
+	};
 
 	class Parser {
 
 		private:
 
 		String fileName = "";
+
 		String * inputFile = nullptr;
-		
+
 		ArrayList<Token> * tokens = nullptr;
+
+		ArrayList<SyntaxError> * errors = new ArrayList<SyntaxError>();
 
 		SizeType index = 0;
 
 		inline Expression * expression() {
-			try {
-				return equality();
-			} catch (Exception & e) { throw; }
+			return equality();
 		}
 
 		Expression * equality() {
-			Expression * ex = nullptr;
-			try {
-				ex = comparison();
-			} catch (Exception & e) { throw; }
+			Expression * ex = comparison();
 			ArrayList<String> * ops = new ArrayList<String>();
 			ops -> push("=="); ops -> push("!=");
-			try {
-				while (match(TokenType::infixOperator)
-					&& matchOperators(ops)) {
-					Token * op = new Token();
-					* op = previous();
-					Expression * rs = comparison();
-					ex = new Binary(ex, op, rs);
-				}
-			} catch (Exception & e) {
-				delete ops; throw;
+			while (match(TokenType::infixOperator) &&
+				   matchOperators(ops)) {
+				Token * op = new Token(previous());
+				Expression * rs = comparison();
+				ex = new Binary(ex, op, rs);
 			}
 			delete ops;
 			return ex;
@@ -67,22 +83,14 @@ namespace Stack {
 
 		/*  Parses Low Priority infix Operators [N + M]. */
 		Expression * lowPriorityOperator() {
-			Expression * ex = nullptr;
-			try {
-				ex = mediumPriorityOperator();
-			} catch (Exception & e) { throw; }
+			Expression * ex = mediumPriorityOperator();
 			ArrayList<TokenType> * ops = new ArrayList<TokenType>();
 			ops -> push(TokenType::plus);
 			ops -> push(TokenType::minus);
-			try {
-				while (match(ops)) {
-					Token * op = new Token();
-					* op = previous();
-					Expression * rs = mediumPriorityOperator();
-					ex = new Binary(ex, op, rs);
-				}
-			} catch (Exception & e) {
-				delete ops; throw;
+			while (match(ops)) {
+				Token * op = new Token(previous());
+				Expression * rs = mediumPriorityOperator();
+				ex = new Binary(ex, op, rs);
 			}
 			delete ops;
 			return ex;
@@ -90,23 +98,15 @@ namespace Stack {
 
 		/*  Parses Medium Priority infix Operators [N * M]. */
 		Expression * mediumPriorityOperator() {
-			Expression * ex = nullptr;
-			try {
-				ex = highPriorityOperator();
-			} catch (Exception & e) { throw; }
+			Expression * ex = highPriorityOperator();
 			ArrayList<TokenType> * ops = new ArrayList<TokenType>();
 			ops -> push(TokenType::star);
 			ops -> push(TokenType::slash);
 			ops -> push(TokenType::modulus);
-			try {
-				while (match(ops)) {
-					Token * op = new Token();
-					* op = previous();
-					Expression * rs = highPriorityOperator();
-					ex = new Binary(ex, op, rs);
-				}
-			} catch (Exception & e) {
-				delete ops; throw;
+			while (match(ops)) {
+				Token * op = new Token(previous());
+				Expression * rs = highPriorityOperator();
+				ex = new Binary(ex, op, rs);
 			}
 			delete ops;
 			return ex;
@@ -119,56 +119,46 @@ namespace Stack {
 			ops -> push(TokenType::plus);
 			ops -> push(TokenType::exclamationMark);
 			ops -> push(TokenType::tilde);
-			try {
-				if (match(ops)) {
-					Token * op = new Token();
-					* op = previous();
-					Expression * rs = highPriorityOperator();
-					delete ops;
-					return new Unary(op, rs);
-				}
-			} catch (Exception & e) {
-				delete ops; throw;
+			if (match(ops)) {
+				Token * op = new Token(previous());
+				Expression * rs = highPriorityOperator();
+				delete ops;
+				return new Unary(op, rs);
 			}
 			delete ops;
-			try {
-				return primary();
-			} catch(Exception & e) { throw; }
+			return primary();
 		}
 
 		/*  Parses Nested Expressions and Literals. */
 		Expression * primary() {
 			Token t = peek();
-			try {
-				if (t.isTypeLiteral()) {
-					advance();
-					Token * literal = new Token(t);
-					return new Literal(literal);
-				} else if (match(TokenType::openRoundBracket)) {
-					Expression * ex = expression();
-					consume(TokenType::closeRoundBracket);
-					return new Grouping(ex);
+			if (t.isTypeLiteral()) {
+				advance();
+				Token * literal = new Token(t);
+				return new Literal(literal);
+			} else if (match(TokenType::openRoundBracket)) {
+				Expression * ex = expression();
+				try {
+					consume(TokenType::closeRoundBracket, ")");
+				} catch (SyntaxError & s) {
+					errors -> push(s);
+					if (ex != nullptr) delete ex;
+					synchronise();
 				}
-			} catch (Exception & e) { throw;}
-			Linker::FilePosition fp = Linker::getPosition(inputFile, t.position);
-			throw UnexpectedEndException(t.lexeme, fp, fileName);
+				return new Grouping(ex);
+			}
+			return nullptr;
 		}
 
 		Expression * comparison() {
-			Expression * ex = nullptr;
-			try {
-				ex = lowPriorityOperator();
-			} catch (Exception & e) { throw; }
+			Expression * ex = lowPriorityOperator();
 			ArrayList<String> * ops = new ArrayList<String>();
 			ops -> push(">"); ops -> push("<");
 			ops -> push(">="); ops -> push(">=");
-			while (matchOperators(ops)) {
-				Token * op = new Token();
-				* op = previous();
-				Expression * rs = nullptr;
-				try {
-					rs = lowPriorityOperator();
-				} catch (Exception & e) { throw; }
+			while (match(TokenType::infixOperator) &&
+				   matchOperators(ops)) {
+				Token * op = new Token(previous());
+				Expression * rs = lowPriorityOperator();
 				ex = new Binary(ex, op, rs);
 			}
 			delete ops;
@@ -176,60 +166,44 @@ namespace Stack {
 		}
 
 		Boolean matchOperator(String & op) {
-			try {
-				if (checkOperator(op)) {
-					advance();
-					return true;
-				} return false;
-			} catch (Exception & e) { throw; }
+			if (checkOperator(op)) {
+				advance();
+				return true;
+			} return false;
 		}
 
 		Boolean matchOperators(ArrayList<String> * operators) {
-			try {
-				for (String & op : * operators) {
-					if (checkOperator(op)) {
-						advance();
-						return true;
-					}
-				} return false;
-			} catch (Exception & e) { throw; }
+			for (String & op : * operators) {
+				if (checkOperator(op)) {
+					advance();
+					return true;
+				}
+			} return false;
 		}
 
 		Boolean match(TokenType type) {
-			try {
-				if (check(type)) {
-					advance();
-					return true;
-				} return false;
-			} catch (Exception & e) { throw; }
+			if (check(type)) {
+				advance();
+				return true;
+			} return false;
 		}
 
 		Boolean match(ArrayList<TokenType> * types) {
-			try {
-				for (TokenType & type : * types) {
-					if (check(type)) {
-						advance();
-						return true;
-					}
-				} return false;
-			} catch (Exception & e) { throw; }
+			for (TokenType & type : * types) {
+				if (check(type)) {
+					advance();
+					return true;
+				}
+			} return false;
 		}
 
 		Boolean checkOperator(String op) {
-			if (isOutOfRange()) {
-				Linker::FilePosition fp = Linker::getPosition(inputFile,
-															  previous().position);
-				throw UnexpectedEndException(previous().lexeme, fp, fileName);
-			}
+			if (isAtEnd()) return false;
 			return peek().lexeme == op;
 		}
 
 		Boolean check(TokenType type) {
-			if (isOutOfRange()) {
-				Linker::FilePosition fp = Linker::getPosition(inputFile,
-															  previous().position);
-				throw UnexpectedEndException(previous().lexeme, fp, fileName);
-			}
+			if (isAtEnd()) return false;
 			return peek().type == type;
 		}
 
@@ -240,9 +214,7 @@ namespace Stack {
 		}
 
 		Boolean isAtEnd() {
-			if (tokens -> size() <= index + 1) return true;
-			if (tokens -> size() == 0) return true;
-			return false;
+			return peek().type == TokenType::endFile;
 		}
 
 		Token peek() { return tokens -> at(index); }
@@ -250,16 +222,19 @@ namespace Stack {
 		Token previous() { return tokens -> at(index - 1); }
 
 		Token advance() {
-			//if (!isAtEnd()) index++;
-			index += 1;
+			if (!isAtEnd()) index += 1;
 			return previous();
 		}
 
-		Token consume(TokenType type) {
+		Token consume(TokenType type, String lexeme = "") {
 			Token t = peek();
 			if (check(type)) return advance();
 			Linker::FilePosition fp = Linker::getPosition(inputFile, t.position);
-			throw SyntaxErrorException(t.lexeme, ")", fp, fileName);
+			throw SyntaxError(
+				(lexeme.length() > 0 ? "Expected '" + lexeme +
+				"' but found '" + t.lexeme + "'!" :
+				"Expecting a different token than '" + t.lexeme + "'!"), fp
+			);
 		}
 
 		void synchronise() {
@@ -326,11 +301,22 @@ namespace Stack {
 						   String * i = nullptr,
 						   String f = "Unknown File") {
 			if (t == nullptr) return nullptr;
-			if (t -> size() == 0) throw EmptyUnitException(f);
-			tokens = t; inputFile = i; fileName = f;
+			if (t -> size() < 2) return nullptr;
 			try {
-				return expression();
-			} catch (Exception & e) { throw; }
+				consume(TokenType::beginFile, "beginFile");
+			} catch (SyntaxError & s) {
+				errors -> push(s);
+				errors -> shrinkToFit();
+				throw ParseErrorException(errors, fileName);
+			}
+			tokens = t; inputFile = i; fileName = f;
+			Expression * ex = expression();
+			if (errors -> size() > 0) {
+				errors -> shrinkToFit();
+				throw ParseErrorException(errors, fileName);
+			}
+			delete errors;
+			return ex;
 		}
 
 	};
