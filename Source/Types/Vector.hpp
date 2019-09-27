37dd4fef965dd92cb62ee96e0dfdcdec4779895c
Cristian-A <cristianantonuccio@icloud.com>
2019-09-27 15:48:26 +0200

    Preparation for vector class

---
+++ Source/Types/Vector.hpp
@@ -0,0 +1,30 @@
+
+/*!
+ *
+ *    + --------------------------------------- +
+ *    |  Vector.hpp                             |
+ *    |                                         |
+ *    |               Vector Type               |
+ *    |                                         |
+ *    |  Created by Cristian A.                 |
+ *    |  Copyright © MIT. All rights reserved.  |
+ *    + --------------------------------------- +
+ *
+ *    Note: This software is licensed under
+ *          the (MIT) Massachusetts Institute
+ *          of Technology License.
+ *
+ */
+
+#include "../Aliases/Prototypes.hpp"
+
+#ifndef STACKVECTOR
+#define STACKVECTOR
+
+namespace Stack {
+
+	
+
+}
+
+#endif
